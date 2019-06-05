5ac21d2119b07cb22b9027904b8045fa91432028
Ligang.Wang <wlgchun@163.com>
2019-06-05 23:58:31 +0800

    update

--- src/main/java/com/wlgdo/webiot/WebiotApplication.java
+++ src/main/java/com/wlgdo/webiot/WebiotApplication.java
@@ -27,8 +27,8 @@ public class WebiotApplication {
     @PostConstruct
     public void init() throws Exception {
         System.out.println("wlgdo web netty service is initting...");
-        WebIoTNettyServer.init();
-        WebSocketServer.init();
+        /*WebIoTNettyServer.init();
+        WebSocketServer.init();*/
         System.out.println("wlgdo web netty server init successful !!!");
     }
 
