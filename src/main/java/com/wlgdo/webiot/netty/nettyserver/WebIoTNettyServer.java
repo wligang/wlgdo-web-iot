b5a713d9fda1ec01e4503dba30aedf699d9cb03b
Ligang.Wang <wlgchun@163.com>
2019-08-31 23:53:53 +0800

    code review

--- src/main/java/com/wlgdo/webiot/netty/nettyserver/WebIoTNettyServer.java
+++ src/main/java/com/wlgdo/webiot/netty/nettyserver/WebIoTNettyServer.java
@@ -1,6 +1,5 @@
 package com.wlgdo.webiot.netty.nettyserver;
 
-
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
@@ -10,19 +9,20 @@ import io.netty.handler.codec.LengthFieldPrepender;
 import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.codec.string.StringEncoder;
 import io.netty.util.CharsetUtil;
+import org.springframework.beans.factory.annotation.Value;
 
 /***
- *
- * @ClassName: WebIoTNettyServer
  * @Description: netty service
  * @author Ligang.Wang[wlgchun@163.com]
  * @date 2019年3月14日
  */
 public class WebIoTNettyServer {
     //ip 地址
-    private static String IP = "0.0.0.0";
+    @Value("netty.server.ip")
+    private static final String IP = null;
     //默认端口
-    private static int PORT = 9000;
+    @Value("netty.server.prot")
+    private static final int PORT = 0;
 
     private static final int BIZGROUPSIZE = Runtime.getRuntime().availableProcessors() * 2;
 
@@ -52,6 +52,7 @@ public class WebIoTNettyServer {
         });
         System.out.println("【TCP服务器IP】" + IP + "【TCP服务器PORT】" + PORT);
 
+        //此处启动netty服务
         Thread thread = new Thread() {
             @Override
             public void run() {
