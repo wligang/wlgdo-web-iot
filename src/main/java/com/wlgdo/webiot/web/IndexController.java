5cfe3dac4f5c2b9d992f4b0793dd1c36d8dc26f6
Ligang.Wang <wlgchun@163.com>
2019-08-31 23:46:44 +0800

    code review

--- src/main/java/com/wlgdo/webiot/web/IndexController.java
+++ src/main/java/com/wlgdo/webiot/web/IndexController.java
@@ -3,6 +3,8 @@ package com.wlgdo.webiot.web;
 import com.wlgdo.webiot.netty.NettyGlobalProp;
 import io.netty.channel.Channel;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,39 +21,46 @@ import java.util.Date;
  * @author : Ligang.Wang[wangligang@karaku.cn]
  * @date : 2019/3/14
  */
-
+@Slf4j
 @RestController
+@AllArgsConstructor
 public class IndexController {
 
-    Logger logger = LoggerFactory.getLogger(getClass());
 
     @Autowired
     IndexService indexService;
 
-
+    /**
+     * 统计有多少个连接
+     *
+     * @param request
+     * @return
+     */
     @RequestMapping(value = "index", method = RequestMethod.GET)
     public Object index(HttpServletRequest request) {
-        logger.info(" The index page have been invist");
-        logger.info("{}:{}", request.getRemoteHost(), request.getRemotePort());
-
+        log.info("The index page have been invist{}:{}", request.getRemoteHost(), request.getRemotePort());
         String welcomeHtml = "<html><h1>Wellcome to wlgdo web netty !!!<br>###</h1></html>";
-
         int terminalNum = NettyGlobalProp.getInstance().getBiMap().size();
-
         return welcomeHtml.replace("###", String.format("目前链接<b>[%s]</b>个终端", terminalNum));
     }
 
+    /**
+     * 向终端发送消息
+     *
+     * @param msg
+     * @param id
+     * @return
+     */
     @RequestMapping(value = "send/{id}/{msg}", method = RequestMethod.GET)
     public Object send(@PathVariable String msg, @PathVariable Integer id) {
-        logger.info("begin send message to client:{},{}", id, msg);
+        log.info("begin send message to client:{},{}", id, msg);
         if (NettyGlobalProp.getInstance().getBiMap().containsKey(id)) {
             Channel channle = NettyGlobalProp.getInstance().getBiMap().get(id);
             channle.writeAndFlush(msg);
             channle.writeAndFlush(new TextWebSocketFrame(String.format("服务器收到消息[%s]:%s",
-                    new SimpleDateFormat("HH:mm:ss").format(new Date()),msg)));
+                    new SimpleDateFormat("HH:mm:ss").format(new Date()), msg)));
             return "successful";
         }
         return "failure";
-
     }
 }
